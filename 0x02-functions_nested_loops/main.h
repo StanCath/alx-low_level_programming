e1602ea8a7c94ce5e126bbd11dd8604efa999da7
Stanley <san.cathline@gmail.com>
2022-06-07 09:08:58 -0700

    print alphabets

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,2 @@
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-
-    print_alphabet();
-
-    return (0);
-
-}
+int _putchar(char c);
+void print_alphabet(void);
