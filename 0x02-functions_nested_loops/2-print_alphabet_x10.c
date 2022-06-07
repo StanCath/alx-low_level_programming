e532a544c2982dbdeec9353f34cd112f5f8e4948
Stanley <san.cathline@gmail.com>
2022-06-07 09:26:22 -0700

    print alphabets 10*

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,7 @@
 #include"main.h"
 
 /**
- * main - Entry point
- *
  * print_alphabet_x10 - function that will print the alphabet 10 times
- *
- * Return: Always 0.
 */
 
 void print_alphabet_x10(void)
